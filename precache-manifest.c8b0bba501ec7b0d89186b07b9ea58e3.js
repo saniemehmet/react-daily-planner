7befc82edad01ec3f988384f7796cd1dffeeb86d
bgcodinggirl <sanie_1998@abv.bg>
2020-06-10 22:00:37 +0300

    Updates

---
+++ precache-manifest.c8b0bba501ec7b0d89186b07b9ea58e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf7215f2ade070f3d8726f9873fe8f4",
+    "url": "/bgcodinggirl/react-daily-planner/index.html"
+  },
+  {
+    "revision": "e8fcd02e022b6946e32a",
+    "url": "/bgcodinggirl/react-daily-planner/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ccdf5afd916ff6085aa1",
+    "url": "/bgcodinggirl/react-daily-planner/static/css/main.da8839ed.chunk.css"
+  },
+  {
+    "revision": "e8fcd02e022b6946e32a",
+    "url": "/bgcodinggirl/react-daily-planner/static/js/2.6b3221d5.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/bgcodinggirl/react-daily-planner/static/js/2.6b3221d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccdf5afd916ff6085aa1",
+    "url": "/bgcodinggirl/react-daily-planner/static/js/main.5534f4b2.chunk.js"
+  },
+  {
+    "revision": "ae30875f5c0fef578d78",
+    "url": "/bgcodinggirl/react-daily-planner/static/js/runtime-main.94c3298f.js"
+  }
+]);
